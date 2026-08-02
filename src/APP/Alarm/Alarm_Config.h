af0d3d2e6188b11e77c32578a1557206be84263e
Yasmine <yasmine2software@gmail.com>
2026-08-03 00:47:43 +0300

    unused alarm, dup of red led

--- src/APP/Alarm/Alarm_Config.h
+++ src/APP/Alarm/Alarm_Config.h
@@ -1,10 +1,6 @@
 #ifndef _ALARM_CONFIG_H
 #define _ALARM_CONFIG_H
 
-#include "../../MCAL/DIO/DIO_Interface.h"
-
-#define Alarm_WarnLed_Group Dio_GroupA
-#define Alarm_WarnLed_Pin   Dio_Pin3
 #define Alarm_ShortBeepMs   300
 
 #endif
--- src/APP/Alarm/Alarm_Program.c
+++ src/APP/Alarm/Alarm_Program.c
@@ -2,12 +2,10 @@
 #include "Alarm_Interface.h"
 #include "Alarm_Private.h"
 #include "../../HAL/BUZZER/BUZZER_Interface.h"
-#include "../../HAL/LED/LED_Interface.h"
 
 void Alarm_Init(void)
 {
     Buzzer_Init();
-    Led_Init(Alarm_WarnLed_Group, Alarm_WarnLed_Pin);
 }
 
 void Alarm_ShortBeep(void)
@@ -21,7 +19,6 @@ void Alarm_LockoutAlert(uint16_t Seconds)
 {
     uint16_t i;
 
-    Led_on(Alarm_WarnLed_Group, Alarm_WarnLed_Pin, SourceConnection);
     Alarm_ShortBeep();
     _delay_ms(150);
     Alarm_ShortBeep();
@@ -30,6 +27,4 @@ void Alarm_LockoutAlert(uint16_t Seconds)
     {
         _delay_ms(1000);
     }
-
-    Led_off(Alarm_WarnLed_Group, Alarm_WarnLed_Pin, SourceConnection);
 }
