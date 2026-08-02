48b0d0572f6eb24d2c3eb81595cf7d728b48c9b1
Yasmine <yasmine2software@gmail.com>
2026-08-02 19:15:14 +0300

    add keypad remapping

--- src/HAL/KEYPAD/KYP_Config.h
+++ src/HAL/KEYPAD/KYP_Config.h
@@ -5,20 +5,20 @@
 //init
 #define KPD_ROW_PINS1_init    DIO_InitPin(Dio_GroupB,Dio_Pin4,Output);
 #define KPD_ROW_PINS2_init    DIO_InitPin(Dio_GroupB,Dio_Pin5,Output);
-#define KPD_ROW_PINS3_init   DIO_InitPin(Dio_GroupB,Dio_Pin6,Output);
-#define KPD_ROW_PINS4_init
-#define KPD_COLUM_PINS1_init     DIO_InitPin(Dio_GroupB,Dio_Pin0,Input);
-#define KPD_COLUM_PINS2_init     DIO_InitPin(Dio_GroupB,Dio_Pin1,Input);
-#define KPD_COLUM_PINS3_init     DIO_InitPin(Dio_GroupB,Dio_Pin2,Input);
-#define KPD_COLUM_PINS4_init     DIO_InitPin(Dio_GroupB,Dio_Pin3,Input);
+#define KPD_ROW_PINS3_init    DIO_InitPin(Dio_GroupB,Dio_Pin6,Output);
+#define KPD_ROW_PINS4_init    DIO_InitPin(Dio_GroupB,Dio_Pin7,Output);
+
+#define KPD_COLUM_PINS1_init     DIO_InitPin(Dio_GroupD,Dio_Pin2,Input);
+#define KPD_COLUM_PINS2_init     DIO_InitPin(Dio_GroupD,Dio_Pin3,Input);
+#define KPD_COLUM_PINS3_init     DIO_InitPin(Dio_GroupD,Dio_Pin4,Input);
 //write
-#define KPD_ROW_PINS1_write      DIO_WritePin(Dio_GroupD,Dio_Pin0,High); 
-#define KPD_ROW_PINS2_write     DIO_WritePin(Dio_GroupD,Dio_Pin1,High);
-#define KPD_ROW_PINS3_write       DIO_WritePin(Dio_GroupD,Dio_Pin2,High);
-#define KPD_ROW_PINS4_write
-#define KPD_COLUM_PINS1_write   DIO_WritePin(Dio_GroupB,Dio_Pin0,High);
-#define KPD_COLUM_PINS2_write       DIO_WritePin(Dio_GroupB,Dio_Pin1,High);
-#define KPD_COLUM_PINS3_write       DIO_WritePin(Dio_GroupB,Dio_Pin2,High);
-#define KPD_COLUM_PINS4_write       DIO_WritePin(Dio_GroupB,Dio_Pin3,High);
+#define KPD_ROW_PINS1_write      DIO_WritePin(Dio_GroupB,Dio_Pin4,High);
+#define KPD_ROW_PINS2_write      DIO_WritePin(Dio_GroupB,Dio_Pin5,High);
+#define KPD_ROW_PINS3_write      DIO_WritePin(Dio_GroupB,Dio_Pin6,High);
+#define KPD_ROW_PINS4_write      DIO_WritePin(Dio_GroupB,Dio_Pin7,High);
+
+#define KPD_COLUM_PINS1_write    DIO_WritePin(Dio_GroupD,Dio_Pin2,High);
+#define KPD_COLUM_PINS2_write    DIO_WritePin(Dio_GroupD,Dio_Pin3,High);
+#define KPD_COLUM_PINS3_write    DIO_WritePin(Dio_GroupD,Dio_Pin4,High);
 
 #endif
--- src/HAL/KEYPAD/KYP_Programe.c
+++ src/HAL/KEYPAD/KYP_Programe.c
@@ -1,6 +1,12 @@
 #include"KYP_Interface.h"
 
-static uint8_t keypad[4][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'},{'*','0','#'}};
+static uint8_t keypad[4][3]={
+    {'1','2','3'},
+    {'4','5','6'},
+    {'7','8','9'},
+    {'*','0','#'}
+};
+
 void KPD_Init(){
     //colum are 3
     // DIO_InitPin(Dio_GroupB,Dio_Pin4,Output);
@@ -22,43 +28,43 @@ void KPD_Init(){
     // DIO_WritePin(Dio_GroupB,Dio_Pin2,High);
     // DIO_WritePin(Dio_GroupB,Dio_Pin3,High);
 
-    KPD_ROW_PINS1_init;  
-    KPD_ROW_PINS2_init; 
-    KPD_ROW_PINS3_init;  
+
+    KPD_ROW_PINS1_init;
+    KPD_ROW_PINS2_init;
+    KPD_ROW_PINS3_init;
+    KPD_ROW_PINS4_init;
     KPD_COLUM_PINS1_init;
     KPD_COLUM_PINS2_init;
     KPD_COLUM_PINS3_init;
-    KPD_COLUM_PINS4_init;
 
-    KPD_ROW_PINS1_write; 
-    KPD_ROW_PINS2_write; 
-    KPD_ROW_PINS3_write; 
+    KPD_ROW_PINS1_write;
+    KPD_ROW_PINS2_write;
+    KPD_ROW_PINS3_write;
+    KPD_ROW_PINS4_write;
     KPD_COLUM_PINS1_write;
     KPD_COLUM_PINS2_write;
     KPD_COLUM_PINS3_write;
-    KPD_COLUM_PINS4_write;
-
 }
-#define Pressed 0 
+#define Pressed 0
 #define Not_Pressed 0xFF
 
 uint8_t KPD_GetButtonValue()
 {
     uint8_t Result= 0 ;
-    for(uint8_t C = 0 ;C <3;C++)
+    for(uint8_t R = 0 ;R<4;R++)
     {
-        DIO_WritePin(Dio_GroupD,C,Low);
-        for(uint8_t R = 0 ;R<4;R++)
+        DIO_WritePin(Dio_GroupB,Dio_Pin4+R,Low);
+        for(uint8_t C = 0 ;C <3;C++)
         {
-            DIO_ReadPin(Dio_GroupB,R,&Result);
+            DIO_ReadPin(Dio_GroupD,Dio_Pin2+C,&Result);
             if(Result==Pressed)
             {
-                DIO_WritePin(Dio_GroupB,R,High);
+                DIO_WritePin(Dio_GroupB,Dio_Pin4+R,High);
                 _delay_ms(100);
                 return keypad[R][C];
             }
         }
-        DIO_WritePin(Dio_GroupD,C,High);
+        DIO_WritePin(Dio_GroupB,Dio_Pin4+R,High);
     }
   return Not_Pressed;
 }
